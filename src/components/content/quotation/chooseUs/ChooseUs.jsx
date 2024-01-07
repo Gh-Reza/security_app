995b21f72b0db51d19ffbfb218cdd27abeaa8dc6
reza <ghrezam97@gmail.com>
2024-01-07 21:09:11 +0430

    Add quotation components

---
+++ src/components/content/quotation/chooseUs/ChooseUs.jsx
@@ -0,0 +1,12 @@
+import Title from './Title';
+
+import Reasons from './Reason';
+
+export default function ChooseUs() {
+  return (
+    <div className="w-full px-3 choose-wrapper lg:w-1/2">
+      <Title />
+      <Reasons />
+    </div>
+  );
+}
---
+++ src/components/content/quotation/chooseUs/Title.jsx
@@ -0,0 +1,16 @@
+export default function Title() {
+  return (
+    <div className="title-wrapper mb-[30px]">
+      <h2
+        className="text-[#1d2736] text-3xl leading-10 font-bold
+                     pb-3 relative before:absolute before:bottom-0 before:left-0
+                     before:w-[120px] before:h-[2px] before:bg-brand_color
+                     after:absolute after:bottom-0 after:left-0
+                     after:w-[60px] after:h-[2px] after:bg-[#333]
+                    "
+      >
+        Why Choose Us
+      </h2>
+    </div>
+  );
+}
