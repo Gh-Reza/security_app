ec4bd66cc444711bf59e3344ea91a7e485361416
reza <ghrezam97@gmail.com>
2024-01-10 18:44:14 +0430

    Create Home component

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,21 +1,9 @@
-import Services from './services/Services';
-import Benefits from './benefits/Benefits';
-import OurServices from './ourServices/OurServices';
-import Quotation from './quotation/Quotation';
-import OurClients from './ourClients/OurClients';
-import Blogs from './blog/Blogs';
-import Hero from '../hero/Hero';
+import Home from '../pages/Home';
 
 export default function Content() {
   return (
     <main className="relative page_content">
-      <Hero />
-      <Services />
-      <Benefits />
-      <OurServices />
-      <Quotation />
-      <OurClients />
-      <Blogs />
+      <Home />
     </main>
   );
 }
