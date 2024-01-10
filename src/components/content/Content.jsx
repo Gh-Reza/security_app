e2cf1bc9163ff48d877c9245bcd04b20c67d7ad8
reza <ghrezam97@gmail.com>
2024-01-10 18:38:46 +0430

    Remove Hero component from App.jsx and add Hero component to Content.jsx

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -4,10 +4,12 @@ import OurServices from './ourServices/OurServices';
 import Quotation from './quotation/Quotation';
 import OurClients from './ourClients/OurClients';
 import Blogs from './blog/Blogs';
+import Hero from '../hero/Hero';
 
 export default function Content() {
   return (
     <main className="relative page_content">
+      <Hero />
       <Services />
       <Benefits />
       <OurServices />
