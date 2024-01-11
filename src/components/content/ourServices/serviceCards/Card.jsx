5fa499e8e03a4c2b56582eeee22c380af411d3ac
reza <ghrezam97@gmail.com>
2024-01-11 08:33:58 +0430

    Add services page

--- src/components/content/ourServices/serviceCards/Card.jsx
+++ src/components/content/ourServices/serviceCards/Card.jsx
@@ -4,36 +4,38 @@ export default function Card({ data }) {
   return (
     <div className="service_card_wrapper">
       <div className="px-4 mb-[30px]">
-        <div className="thumb">
-          {/* FIXME: href value */}
-          <a href="https" className="block">
-            <img className="w-full" src={data.img} alt="service img" />
-          </a>
-        </div>
-        <div className="content py-[30px]
-                        px-[20px] text-center
-                        text-[#575757]
-                        bg-white
-                      "
-        >
-          <h4 className="mb-[13px] px-[9px] font-semibold
-                      hover:text-brand_color transition-all duration-[0.5s]
+        <div className="transition-shadow duration-500 hover:shadow-md hover:shadow-gray-200">
+          <div className="thumb">
+            {/* FIXME: href value */}
+            <a href="https" className="block">
+              <img className="w-full" src={data.img} alt="service img" />
+            </a>
+          </div>
+          <div className="content py-[30px]
+                          px-[20px] text-center
+                          text-[#575757]
+                          bg-white
                         "
           >
-            <a href={data.link}>{ data.title }</a>
-          </h4>
-          <p className="text-[#818181] text-[14px] mb-2">
-            { data.description }
-          </p>
-          <a
-            href={data.link}
-            className="px-[42px] py-3 font-semibold
-                                hover:text-brand_color
-                                transition-all duration-[0.5s]
-                                "
-          >
-            READ MORE
-          </a>
+            <h4 className="mb-[13px] px-[9px] font-semibold
+                        hover:text-brand_color transition-all duration-[0.5s]
+                          "
+            >
+              <a href={data.link}>{ data.title }</a>
+            </h4>
+            <p className="text-[#818181] text-[14px] mb-2">
+              { data.description }
+            </p>
+            <a
+              href={data.link}
+              className="px-[42px] py-3 font-semibold
+                                  hover:text-brand_color
+                                  transition-all duration-[0.5s]
+                                  "
+            >
+              READ MORE
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,7 +1,79 @@
+import CommonHero from '../hero/CommonHero';
+import service01 from '../../assets/images/services/service-01.jpg';
+import Card from '../content/ourServices/serviceCards/Card';
+import MainSectionTitle from '../common/MainSectionTitle';
+
 export default function Services() {
+  const cardCollectionInfo = [
+    {
+      img: service01,
+      id: 1,
+      title: 'Private Security',
+      description:
+        'Lorem ipsum dolor sit amet ectetur adipelitl sed do eiusmod tempor incidid',
+      link: '#',
+    },
+    {
+      img: service01,
+      id: 2,
+      title: 'Private Security',
+      description:
+        'Lorem ipsum dolor sit amet ectetur adipelitl sed do eiusmod tempor incidid',
+      link: '#',
+    },
+    {
+      img: service01,
+      id: 3,
+      title: 'Private Security',
+      description:
+        'Lorem ipsum dolor sit amet ectetur adipelitl sed do eiusmod tempor incidid',
+      link: '#',
+    },
+    {
+      img: service01,
+      id: 4,
+      title: 'Private Security',
+      description:
+        'Lorem ipsum dolor sit amet ectetur adipelitl sed do eiusmod tempor incidid',
+      link: '#',
+    },
+    {
+      img: service01,
+      id: 5,
+      title: 'Private Security',
+      description:
+        'Lorem ipsum dolor sit amet ectetur adipelitl sed do eiusmod tempor incidid',
+      link: '#',
+    },
+    {
+      img: service01,
+      id: 6,
+      title: 'Private Security',
+      description:
+        'Lorem ipsum dolor sit amet ectetur adipelitl sed do eiusmod tempor incidid',
+      link: '#',
+    },
+    {
+      img: service01,
+      id: 7,
+      title: 'Private Security',
+      description:
+        'Lorem ipsum dolor sit amet ectetur adipelitl sed do eiusmod tempor incidid',
+      link: '#',
+    },
+  ];
+
   return (
-    <main className="relative page_content">
-      <h1>Services</h1>
-    </main>
+    <section className="bg-gray-50">
+      <CommonHero text="Services" />
+      <div className="py-24 wrapper">
+        <MainSectionTitle title="Our Services" />
+        <div className="grid md:grid-cols-2 lg:grid-cols-3">
+          {cardCollectionInfo.map((card) => (
+            <Card data={card} key={card.id} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
