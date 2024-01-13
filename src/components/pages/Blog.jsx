a4088176dc5b7bfb74d3f1f7f1605160785cac77
reza <ghrezam97@gmail.com>
2024-01-13 21:24:52 +0430

    Add blog page

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -1,7 +1,24 @@
+import CommonHero from '../hero/CommonHero';
+import BlogCard from '../content/blog/BlogCard';
+import MainSectionTitle from '../common/MainSectionTitle';
+
 export default function Blog() {
   return (
-    <main className="relative page_content">
-      <h1>Blog</h1>
-    </main>
+    <section>
+      <CommonHero text="Blog" />
+      <div className="py-24 content md:px-12">
+        <MainSectionTitle title="Latest Posts" />
+        <div className="container">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
